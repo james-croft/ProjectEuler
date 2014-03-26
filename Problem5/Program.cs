4f81db8b70b2b826a10579cc2fad8196dbe41a57
James Croft <me@jamescroft.co.uk>
2014-03-26 15:54:57 +0000

    Solved problem 5
    C# solution for problem 5

---
+++ Problem5/Program.cs
@@ -0,0 +1,38 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="James Croft">
+//     Copyright (c) James Croft. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Problem5
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Solution for Project Euler Problem 5 - Smallest multiple
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            // This is extremely slow, will look into alternative method
+            var ans = 1;
+            var i = 1;
+
+            while (i <= 20)
+            {
+                if (ans % i == 0)
+                    i++;
+                else
+                {
+                    ans++;
+                    i = 1;
+                }
+            }
+
+            Console.WriteLine(ans);
+            Console.ReadLine();
+        }
+    }
+}
