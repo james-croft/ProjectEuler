58626368e6a6d431aadb643e65f870ec6b3fe538
James Croft <me@jamescroft.co.uk>
2014-03-27 01:32:06 +0000

    Solved problem 6
    C# console solution for problem 6

---
+++ Problem6/Program.cs
@@ -0,0 +1,27 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="James Croft">
+//     Copyright (c) James Croft. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Problem6
+{
+    using System;
+
+    /// <summary>
+    /// Solution for Project Euler Problem 6 - Sum square difference
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            const int num = 100;
+            var sum = num * (num + 1) / 2;
+            var squared = (num * (num + 1) * (2 * num + 1)) / 6;
+            var ans = sum * sum - squared;
+
+            Console.WriteLine(ans);
+            Console.ReadLine();
+        }
+    }
+}
