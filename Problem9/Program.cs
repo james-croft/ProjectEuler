1259806e5e991613f2c841cf08179b7d4e31672f
James Croft <me@jamescroft.co.uk>
2014-03-28 09:09:44 +0000

    Solved problem 9
    C# console solution for problem 9

---
+++ Problem9/Program.cs
@@ -0,0 +1,47 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="James Croft">
+//     Copyright (c) James Croft. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Problem9
+{
+    using System;
+
+    /// <summary>
+    /// Solution for Project Euler Problem 9 - Special Pythagorean triplet
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            var escape = false;
+            int a;
+            var b = 0;
+            var c = 0;
+
+            for (a = 1; a < 1000 / 3; a++)
+            {
+                for (b = a; b < 1000 / 2; b++)
+                {
+                    c = 1000 - a - b;
+
+                    if (a * a + b * b != c * c) continue;
+
+                    escape = true;
+                    break;
+                }
+
+                if (escape)
+                {
+                    break;
+                }
+            }
+
+            var ans = a * b * c;
+
+            Console.WriteLine(ans);
+            Console.ReadLine();
+        }
+    }
+}
